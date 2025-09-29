03e8918084da56d66626519e968998ac17d33705
rahulhits <rahulhits2013@gmail.com>
2025-09-29 20:38:44 +0530

    Add project files.

---
+++ Repositories/ECommDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+	public class ECommDBContext:DbContext
+	{
+		public ECommDBContext(DbContextOptions<ECommDBContext> options) : base(options)
+		{
+		}
+	}
+}
---
+++ SahosoftMallAPI/Areas/UserManagement/Controllers/PageMasterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SahosoftMallAPI.Areas.UserManagement.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PageMasterController : ControllerBase
+	{
+	}
+}
