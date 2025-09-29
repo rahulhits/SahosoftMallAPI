9e33747222ac5f1be07306bf5bcdfab1740f65a8
rahulhits <rahulhits2013@gmail.com>
2025-09-29 20:45:45 +0530

    FirstDay

--- SahosoftMallAPI/Program.cs
+++ SahosoftMallAPI/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors(Options =>
+{
+	Options.AddPolicy("CorsPolicy", policy =>
+	{
+		policy.AllowAnyOrigin()
+			  .AllowAnyMethod()
+			  .AllowAnyHeader();
+	});
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.UseCors("CorsPolicy");
+app.MapControllers();
 
 app.Run();
